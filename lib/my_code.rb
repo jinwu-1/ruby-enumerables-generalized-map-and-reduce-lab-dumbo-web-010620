85c8bd062825277263abec8e5c07ebf9d304a990
Jin Wu <jinn.wuu@gmail.com>
2019-12-30 18:41:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,9 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  counter = 0
+  while counter < array.length
+    new_array.push(yield(array[counter]))
+    counter += 1
+  end
+  new_array
+end
