12d37909d54989cbbfbed3d551612e6a846a578e
Jin Wu <jinn.wuu@gmail.com>
2019-12-30 18:51:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,18 @@ def map(array)
   end
   new_array
 end
+
+def reduce(array, start_value = nil)
+  if start_value
+    sum = start_value
+    counter = 0
+  else
+    sum = array[0]
+    counter = 1
+  end
+
+  while counter < array.length
+    yield(num_1, array[counter])
+
+
+end
